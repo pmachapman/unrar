cdeedba034b0def4806da3658471cf9562fd9dff
Peter Chapman <peter@conglomo.co.nz>
2019-10-21 16:10:03 +1300

    Updated to 5.8.3

--- version.hpp
+++ version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     5
 #define RARVER_MINOR    80
-#define RARVER_BETA      2
-#define RARVER_DAY      26
-#define RARVER_MONTH     9
+#define RARVER_BETA      3
+#define RARVER_DAY      20
+#define RARVER_MONTH    10
 #define RARVER_YEAR   2019
